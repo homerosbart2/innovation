e7f08ece3d5fee535fc434bdb5e1a2a878280544
Henry Campos <henry.campos@galileo.edu>
2019-01-05 17:42:32 -0600

    First upload

---
+++ assets/js/staff.js
@@ -0,0 +1,32 @@
+var staff = [
+    {
+        name : 'Adrián Catalán',
+        information : 'Innovation Lab leader and Google Developer Expert.',
+        image : 'adrian.jpg',
+        job : 'boss'
+    },
+    {
+        name : 'Luis Jimenez',
+        information : 'Interested in computer vision, hardware and education.',
+        image : 'luis.jpg',
+        job : 'projects-leader'
+    },
+    {
+        name : 'Evelyn Cruz',
+        information : 'Labs and workshops instructor with a huge interest in Biomedic field.',
+        image : 'eve.jpg',
+        job : 'workshops-leader'
+    },
+    {
+        name : 'Sofi Cárdenas',
+        information : 'Working on workshops and projects related to AI and microcontrollers.',
+        image : 'sofi.png',
+        job : 'workshops-assistant'
+    },
+    {
+        name : 'Henry Campos',
+        information : 'Systems engineer, web developer/designer, AI interested and math lover.',
+        image : 'henry.png',
+        job : 'projects-assistant'
+    }
+];
