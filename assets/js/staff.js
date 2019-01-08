6046bf2b7511dc9fb26d0ee7dac79f753b6f9110
Henry Campos <henry.campos@galileo.edu>
2019-01-07 18:43:31 -0600

    Version 0.2.0

--- assets/js/staff.js
+++ assets/js/staff.js
@@ -3,30 +3,51 @@ var staff = [
         name : 'Adrián Catalán',
         information : 'Innovation Lab leader and Google Developer Expert.',
         image : 'adrian.jpg',
-        job : 'boss'
+        job : 'boss',
+        email : 'adriancatalan@galileo.edu'
     },
     {
         name : 'Luis Jimenez',
         information : 'Interested in computer vision, hardware and education.',
         image : 'luis.jpg',
-        job : 'projects-leader'
+        job : 'projects-leader',
+        email : 'luiscu@galileo.edu'
     },
     {
         name : 'Evelyn Cruz',
         information : 'Labs and workshops instructor with a huge interest in Biomedic field.',
-        image : 'eve.jpg',
-        job : 'workshops-leader'
+        image : 'eve.png',
+        job : 'workshops-leader',
+        email : 'evecruz@galileo.edu'
     },
     {
         name : 'Sofi Cárdenas',
-        information : 'Working on workshops and projects related to AI and microcontrollers.',
+        information : 'Currently working on workshops provided by the lab to school students. Her interests include AR, VR, and automation solutions.',
         image : 'sofi.png',
-        job : 'workshops-assistant'
+        job : 'workshops-assistant',
+        email : 'ascardenas@galileo.edu'
     },
     {
         name : 'Henry Campos',
         information : 'Systems engineer, web developer/designer, AI interested and math lover.',
         image : 'henry.png',
-        job : 'projects-assistant'
+        job : 'projects-assistant',
+        email : 'henry.campos@galileo.edu'
+    },
+    {
+        name : 'Fernando López',
+        information : 'Working on workshops with main interest on mechanical design, love to make things work.',
+        image : 'fernando.png',
+        job : 'workshops-assistant',
+        email : 'fernando.lopez@galileo.edu'
+    },
+    {
+        name : 'Jerry Palacios',
+        information : 'Working working working working working.',
+        image : 'jerry.png',
+        job : 'projects-assistant',
+        email : 'jerry.palacios@galileo.edu'
     }
 ];
+
+//Studied Electronical Engineering at Universidad Galileo. Currently working on workshops provided by the lab to school students in order to motivate and guide them towards learning and enjoying to work with different technologies. Her interests include AR, VR, and automation solutions.
